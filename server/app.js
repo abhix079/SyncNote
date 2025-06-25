85204a4dacb7e841b2f724d3503f514a0eb6934d
abhix079 <abhishek788k@gmail.com>
2025-06-26 02:11:49 +0530

    meowww

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 4001;
+const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI ;
 
 
