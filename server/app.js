73ab26fee1c98257448967befa6e96d8f2849e6d
abhix079 <abhishek788k@gmail.com>
2025-06-26 02:13:27 +0530

    meowww

--- server/app.js
+++ server/app.js
@@ -14,7 +14,12 @@ const MONGO_URI = process.env.MONGO_URI ;
 
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:"https://syncnote.onrender.com",
+    credentials:true
+  }
+));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
