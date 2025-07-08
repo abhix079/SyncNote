07eaffa1355ec5568d715ba1e26941c2a1e31c92
abhix079 <abhishek788k@gmail.com>
2025-07-09 00:22:11 +0530

    color-grading

--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -1,34 +1,33 @@
 import styles from "../components/Features.module.css";
 import { MdOutlineCloudSync } from "react-icons/md";
-
 import { LuFileLock2 } from "react-icons/lu";
 import { MdOutlineImportantDevices } from "react-icons/md";
 import { MdColorLens } from "react-icons/md";
 
 const featuresData = [
   {
-    icon: <MdOutlineCloudSync size={50} color="orange" />,
+    icon: <MdOutlineCloudSync size={50} color="#4F46E5" />,
     title: "Cloud Sync",
     desc: [
       "Your notes are automatically synced across all devices in real time.",
     ],
   },
   {
-    icon: <LuFileLock2 size={50} color="orange" />,
+    icon: <LuFileLock2 size={50} color="#4F46E5" />,
     title: "Secure & Private",
     desc: [
       "End-to-end encryption ensures your thoughts remain private and secure in the cloud.",
     ],
   },
   {
-    icon: <MdOutlineImportantDevices size={50} color="orange" />,
+    icon: <MdOutlineImportantDevices size={50} color="#4F46E5" />,
     title: "Cross Platform",
     desc: [
       "Access your notes on web, mobile and desktop apps with seamless experience.",
     ],
   },
   {
-    icon: <MdColorLens size={50} color="orange" />,
+    icon: <MdColorLens size={50} color="#4F46E5" />,
     title: "Custom Themes",
     desc: [
       "Create a comfortable and personalized writing space by switching between light and dark modes.",
